192d6b13cb92bc62fb1b81e5725a710b86af28d4
Daniel Perez <daniel@perez.sh>
2020-01-25 22:54:45 +0000

    Change predictor interface and rename geth

--- ethpred/baseline/geth_predictor.py
+++ ethpred/baseline/geth_predictor.py
@@ -16,41 +16,38 @@ class GethPredictor:
 
     Args:
         min_prices: A mapping of block height to minimum price
+        blocks_count: This number will be multiplied by 5 to obtain
+                      the number of blocks to look behind and divided by 2
+                      to find the number of empty blocks to skip
+            percentile: This is the percentile to use when suggesting the price
     """
-    def __init__(self, min_prices: Dict[int, int]):
+    def __init__(self,
+                 min_prices: Dict[int, int],
+                 blocks_count: int = 20,
+                 percentile: float = 60):
         self.prices = min_prices
+        self.blocks_count = blocks_count
+        self.percentile = min(max(0, percentile), 100)
+        self.max_blocks = self.blocks_count * 5
 
-    def suggest_price(self,
-                      block_number: int,
-                      blocks_count: int = 20,
-                      percentile: float = 60) -> int:
+    def predict_price(self, block_number: int) -> int:
         """Mimics the logic of geth to suggest a gas price
 
         Args:
             block_number: Block number for which the price should be predicted.
                           Block must be in the min_prices give during initialization
-            blocks_count: This number will be multiplied by 5 to obtain
-                          the number of blocks to look behind and divided by 2
-                          to find the number of empty blocks to skip
-            percentile: This is the percentile to use when suggesting the price
         """
-        if percentile <= 0:
-            percentile = 0
-        if percentile >= 100:
-            percentile = 100
-        max_blocks = blocks_count * 5
-        max_empty = blocks_count // 2
-
+        max_empty = self.blocks_count // 2
         prices = []
         current_block_number = block_number - 1
-        while block_number >= 0 and len(prices) < max_blocks:
+        while block_number >= 0 and len(prices) < self.max_blocks:
             price = self.prices[current_block_number]
             if price == 0 and max_empty >= 0:
                 max_empty -= 1
                 continue
             prices.append(price)
             current_block_number -= 1
-        suggested_price = int(np.percentile(prices, q=percentile))
+        suggested_price = int(np.percentile(prices, q=self.percentile))
         if suggested_price >= MAX_PRICE:
             suggested_price = MAX_PRICE
         return suggested_price
---
+++ test/baseline/geth_predictor_test.py
@@ -0,0 +1,17 @@
+import unittest
+
+
+from ethpred.baseline.geth_predictor import GethPredictor
+
+
+class GethPredictorTest(unittest.TestCase):
+    def setUp(self):
+        raw_prices = [int(v * 10 ** 9) for v in [2, 1, 1, 2, 3]]
+        self.min_prices = {i + 100: v for i, v in enumerate(raw_prices)}
+
+    def test_predict_price(self):
+        for percentile, expected in [(60, 2 * 10 ** 9), (100, 3 * 10 ** 9), (20, 10 ** 9)]:
+            with self.subTest(percentile=percentile):
+                predictor = GethPredictor(self.min_prices, blocks_count=1, percentile=percentile)
+                actual = predictor.predict_price(105)
+                self.assertEqual(actual, expected)
